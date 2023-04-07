62a26f95f90caf9a52047aeda74e5a3b7dfbea5f
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2023-04-07 13:18:03 -0600

    Add project files.

---
+++ Python Lists Check/Python_Lists_Check.py
@@ -0,0 +1,6 @@
+
+foods = ["Coctel de Camaron", "Burritos", "Ceviche", "Carne con Chile Rojo", "Chuletas con Chile Verde", "Spaghetti", "Sopa con Queso", "French Fries"]
+print(foods[2])
+foods.append("Tacos")
+for i in range (len(foods)):
+    print(foods)
